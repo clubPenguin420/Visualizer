867b3b1627cf6799d6d8f66f8811db3f42bb278a
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2021-12-02 13:32:17 -0600

    Now I can finally use the GUI designer

--- src/com/fall_sem/Visualizer.java
+++ src/com/fall_sem/Visualizer.java
@@ -1,21 +1,29 @@
 package com.fall_sem;
 
 import javax.swing.*;
-import javax.swing.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Visualizer {
+    private JPanel Base_Panel;
+    private JButton submit;
 
-    private static void initGUI() {
+    public Visualizer() {
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "Deez Nuts");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
         JFrame frame = new JFrame("Visualizer");
+        frame.setContentPane(new Visualizer().Base_Panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-
-        frame.setSize(500, 500);
+        frame.setSize(600, 600);
+        frame.setLocation(1000, 250);
         frame.setVisible(true);
-    }
-    public static void main(String[] args) {
-
-        SwingUtilities.invokeLater(Visualizer::initGUI);
     }
 }
